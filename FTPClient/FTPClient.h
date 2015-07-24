1041490d54549cf78a86e22eee0c74acaba65177
yuneec <yuneec@yuneectekiiMac.local>
2015-07-24 15:46:04 +0800

    init project

---
+++ FTPClient/FTPClient.h
@@ -0,0 +1,13 @@
+//
+//  FTPClient.h
+//  FTPClientDemo
+//
+//  Created by yuneec on 15/7/24.
+//  Copyright (c) 2015年 tbago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTPClient : NSObject
+
+@end
---
+++ FTPClient/FTPClient.m
@@ -0,0 +1,13 @@
+//
+//  FTPClient.m
+//  FTPClientDemo
+//
+//  Created by yuneec on 15/7/24.
+//  Copyright (c) 2015年 tbago. All rights reserved.
+//
+
+#import "FTPClient.h"
+
+@implementation FTPClient
+
+@end
